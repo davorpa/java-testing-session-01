e1d564adf95ca41a444fb7f9226b3f2dd3bfaa30
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-02-21 17:58:03 +0100

    Add tests for Calculadora multiplicar

--- src/test/java/es/seresco/cursojee/CalculadoraMultiplicaTest.java
+++ src/test/java/es/seresco/cursojee/CalculadoraMultiplicaTest.java
@@ -1,8 +1,191 @@
 package es.seresco.cursojee;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 
-@Ignore
 public class CalculadoraMultiplicaTest {
+
+	private Calculadora calculadora;
+
+	/**
+	 * Antes de lanzar cada test
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		calculadora = new Calculadora();
+	}
+
+	/**
+	 * Despues de lanzar cada test
+	 * 
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		calculadora = null;
+	}
+
+	@Test
+	public void testMultiplicaPositivoPorCero() {
+		//Given
+		int num1 = 5;
+		int num2 = 0;
+		// expect
+		int expected = 0;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaPositivoPorCeroComutativa() {
+		//Given
+		int num1 = 5;
+		int num2 = 0;
+		// expect
+		int expected = 0;
+		// do
+		int result = calculadora.multiplicar(num2, num1);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaLimitePositivoPorCero() {
+		//Given
+		int num1 = Integer.MAX_VALUE;
+		int num2 = 0;
+		// expect
+		int expected = 0;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaLimiteNegativoPorCero() {
+		//Given
+		int num1 = Integer.MIN_VALUE;
+		int num2 = 0;
+		// expect
+		int expected = 0;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+	
+
+
+	@Test
+	public void testDoblaLimitePositivo() {
+		//Given
+		int num1 = Integer.MAX_VALUE;
+		int num2 = 2;
+		// expect
+		int expected = -2;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testDoblaLimiteNegativo() {
+		//Given
+		int num1 = Integer.MAX_VALUE;
+		int num2 = -2;
+		// expect
+		int expected = 2;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaDosNumerosPositivos() {
+		//Given
+		int num1 = 5;
+		int num2 = 2;
+		// expect
+		int expected = 10;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaDosNumerosPositivosComutativa() {
+		//Given
+		int num1 = 5;
+		int num2 = 2;
+		// expect
+		int expected = 10;
+		// do
+		int result = calculadora.multiplicar(num2, num1);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaDosNumerosNegativos() {
+		//Given
+		int num1 = -5;
+		int num2 = -2;
+		// expect
+		int expected = 10;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaDosNumerosNegativosComutativa() {
+		//Given
+		int num1 = -5;
+		int num2 = -2;
+		// expect
+		int expected = 10;
+		// do
+		int result = calculadora.multiplicar(num2, num1);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaDosNumerosDistintoSigno() {
+		//Given
+		int num1 = -5;
+		int num2 = 3;
+		// expect
+		int expected = -15;
+		// do
+		int result = calculadora.multiplicar(num1, num2);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
+
+	@Test
+	public void testMultiplicaDosNumerosDistintoSignoComutativa() {
+		//Given
+		int num1 = -5;
+		int num2 = 3;
+		// expect
+		int expected = -15;
+		// do
+		int result = calculadora.multiplicar(num2, num1);
+		// assert
+		Assert.assertEquals(expected, result);
+	}
 
 }
