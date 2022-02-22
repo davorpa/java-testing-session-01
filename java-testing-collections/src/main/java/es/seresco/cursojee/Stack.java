b32d8aa435494906ff4c67a5111bbd08b062fdc4
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-02-22 16:27:43 +0100

    Format Stack + init implementation

--- java-testing-collections/src/main/java/es/seresco/cursojee/Stack.java
+++ java-testing-collections/src/main/java/es/seresco/cursojee/Stack.java
@@ -1,92 +1,78 @@
 package es.seresco.cursojee;
+
 import java.util.Vector;
 
 /**
  * Clase Stack en la que se almacenan datos de la pila.
- * @author Sergio Valdes
- * @see Vector
- * @see Exception
- *
  */
 
 public class Stack {
-	
+
 	/**
 	 * El tamaño de la pila.
 	 */
 	private int longitud;
-	
+
 	/**
 	 * {@link Vector} de {@link Integer} con los elementos de la pila.
 	 */
 	private Vector<Integer> elementos;
-	
+
 	/**
-	 * Constructor por defecto de la clase que se encarga de inicializar
-	 * las propiedades.
+	 * Constructor por defecto de la clase que se encarga de inicializar las
+	 * propiedades.
 	 */
-	public Stack(){
-		elementos = new Vector<Integer>();
+	public Stack() {
+		elementos = new Vector<>();
 		longitud = 0;
 	}
-	
+
 	/**
-	 * Devuelve <code>true</code> si la pila esta vacia y <code>false</code>
-	 * en caso contrario.
+	 * Devuelve <code>true</code> si la pila esta vacia y <code>false</code> en caso
+	 * contrario.
+	 * 
 	 * @return <code>true</code> o <code>false</code>
 	 */
-	public boolean stackVacio(){
-		if (longitud==0) {
-			return true;
-		}
+	public boolean isEmpty() {
 		return false;
 	}
-	
+
 	/**
 	 * Apila o guarda el elemento pasado como parametro a la pila.
+	 * 
 	 * @param o {@link Integer} elemento a añadir
 	 */
-	public void apilar ( Integer o ) {
-		elementos.add(longitud, o);
-		longitud++;
+	public void apilar(Integer o) {
+
 	}
 
 	/**
 	 * Desapila un elemento de la pila.
+	 * 
 	 * @return {@link Integer} elemento obtenido de la pila
 	 */
-	public Integer desapilar () {
-		try {
-			if(stackVacio())
-				throw new ErrorStackVacio();
-			else {
-				return elementos.get(--longitud);
-			}
-		} catch(ErrorStackVacio error) {
-			System.out.println("ERROR: el stack está vacio");
-			return null;
-		}
+	public Integer desapilar() {
+		return null;
 	}
 
 	/**
 	 * Devuelve el numero de elementos de la pila.
+	 * 
 	 * @return numero de elementos
 	 */
-	public int getNumElements() {
-		return longitud;
+	public int size() {
+		return 0;
 	}
 
 	/**
-	 * Excepcion especifica que se produce cuando se realiza 
-	 * una accion sobre una pila vacia.
-	 * @author Sergio Valdes
+	 * Excepcion especifica que se produce cuando se realiza una accion sobre una
+	 * pila vacia.
+	 * 
 	 * @see Exception
-	 *
 	 */
 	@SuppressWarnings("serial")
 	class ErrorStackVacio extends Exception {
-		public ErrorStackVacio() {
-			super();
-		}
+
 	}
+
 }
--- java-testing-collections/src/test/java/es/seresco/cursojee/StackTest.java
+++ java-testing-collections/src/test/java/es/seresco/cursojee/StackTest.java
@@ -4,61 +4,58 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class StackTest {
-	
-	@SuppressWarnings("unused")
+
 	private Stack stackConElementos;
-	@SuppressWarnings("unused")
 	private Stack stackSinElementos;
-	
+
 	/**
-	 * Antes  de lanzar los tests, se  crean dos  pilas,
-	 * una con elementos y otra sin ellos.
+	 * Antes de lanzar los tests, se crean dos pilas, una con elementos y otra sin
+	 * ellos.
+	 * 
 	 * @throws Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
 	}
-	
+
 	/**
-	 * Se verifica que la pila sin elementos devuelve
-	 * <code>true</code> y <code>false</code> para la pila
-	 * con elementos.
+	 * Se verifica que la pila sin elementos devuelve <code>true</code> y
+	 * <code>false</code> para la pila con elementos.
 	 */
 	@Test
 	public void testStackVacio() {
 	}
-	
+
 	/**
-	 * Se añade un elemento a la pila y se verifica que existen
-	 * dos elementos en la misma.
+	 * Se añade un elemento a la pila y se verifica que existen dos elementos en la
+	 * misma.
 	 */
 	@Test
 	public void testApilar() {
 	}
-	
+
 	/**
-	 * Se verifica que al desapilar una pila sin elementos
-	 * devuelve <code>null</code>.
+	 * Se verifica que al desapilar una pila sin elementos devuelve
+	 * <code>null</code>.
 	 */
 	@Test
 	public void testDesapilarStackSinElementos() {
 	}
-	
+
 	/**
-	 * Al desapilar una pila con elementos se reduce el numero
-	 * de elementos de la misma.
+	 * Al desapilar una pila con elementos se reduce el numero de elementos de la
+	 * misma.
 	 */
 	@Test
 	public void testDesapilarStackConElementos() {
 	}
 
 	/**
-	 * Devuelve el numero de elementos de la pila sin elementos y 
-	 * de la pila con elementos.
+	 * Devuelve el numero de elementos de la pila sin elementos y de la pila con
+	 * elementos.
 	 */
 	@Test
 	public void testGetNumElements() {
-		
+
 	}
 }
-
